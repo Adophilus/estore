0b1656f6fda9c66d03660060b08ed9c01496431e
Adophilus <uchenna19of@gmail.com>
2022-06-05 21:53:27 +0100

    added API endpoint for products

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import db from "../../../database.json"
+
+type Data = {
+    "name": string,
+    "cover": string,
+    "images": ["tshirtblack-1.png", "tshirtblack-2.png", "tshirtblack-3.png"],
+    "description": string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      res.status(200).json(db.products.find(product => product.slug === req.query.slug))
+      break
+    default:
+      res.status(405).end()
+      break
+  }
+}
