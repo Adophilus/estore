1ac1e5fa420189912d75480cb7ebd458669767f3
Adophilus <uchenna19of@gmail.com>
2022-06-07 21:36:35 +0100

    added format script

---
+++ format.js
@@ -0,0 +1,26 @@
+const watch = require('node-watch')
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process')
+
+const extensions = ['.html', '.css', '.js', '.jsx', '.tsx', 'md', 'json']
+const prettify = (event, filePath) => {
+  if (event === "update") {
+    for (let extension of extensions) {
+      if (filePath.endsWith(extension) && event === 'update') {
+        console.log(`prettifying ${filePath}`)
+        exec(`yarn format:prettier "${path.join(process.cwd(), filePath)}"`)
+      }
+    }
+  }
+}
+
+
+watch(
+  "./",
+  { recursive: true, filter: (f) => !/node_modules|\.git|\.next|\.public/.test(f) },
+  function (event, filepath) {
+    // console.log('%s changed.', name)
+    prettify(event, filepath)
+  }
+)
