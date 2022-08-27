ce17451d71031ea9410712693191be745c5b1c1f
Adophilus <uchenna19of@gmail.com>
2022-08-27 09:56:30 +0100

    formatted test cases

--- tests/unit/api/server.test.ts
+++ tests/unit/api/server.test.ts
@@ -10,49 +10,7 @@ chai.use(chaiHttp)
 const agent: any = chai.request(process.env.SERVER_BASE_URL)
 const pocketBase = new PocketBase(process.env.POCKETBASE_URL)
 
-// describe('PRODUCT ANALYTICS', () => {
-//   test("check that a product's sale counter is incremented", async () => {
-//     let productStats
-
-//     for (
-//       let i = product['@expand'].stats.trending.counter;
-//       i <= product['@expand'].stats.sale.limit;
-//       i++
-//     ) {
-//       const res = await agent.get(`/api/products/analytics/${productId}`)
-//       expect(res.status).to.equal(200)
-//     }
-
-//     productStats = await pocketBase.Records.getOne(
-//       'product_stats',
-//       product.stats
-//     )
-//     expect(productStats.sale.counter).to.equal(0)
-//   })
-
-//   test(
-//     "check that the product is removed from 'sale'",
-//     () => {
-//       return new Promise((resolve) =>
-//         setTimeout(async () => {
-//           await agent.get(`/api/products/analytics/${productId}`)
-//           const _product = await pocketBase.Records.getOne(
-//             'products',
-//             productId,
-//             {
-//               expand: 'stats'
-//             }
-//           )
-//           expect(_product.onSale).toBeFalsy()
-//           resolve()
-//         }, product['@expand'].stats.sale.duration * 1000)
-//       )
-//     },
-//     product['@expand'].stats.sale.duration * 1000 + 5000
-//   )
-// })
-
-describe('PRODUCT SALES', () => {
+describe('Product sale', () => {
   const testSale = {
     starting: Date.now(),
     duration: 10 * 1000,
@@ -72,7 +30,10 @@ describe('PRODUCT SALES', () => {
   })
 
   test('that the sale has been created', async () => {
-    expect(await agent.put('/api/sales').send(testSale)).toHaveProperty('status', 201)
+    expect(await agent.put('/api/sales').send(testSale)).toHaveProperty(
+      'status',
+      201
+    )
 
     latestSale = (
       await pocketBase.Records.getList('sales', 1, 1, { sort: '-created' })
